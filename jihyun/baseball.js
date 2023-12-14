dbbcf9cdb1270fdc0f40ddff6fd04b34b4ea2b8c
torong <hi_ryuji@naver.com>
2023-12-14 14:19:17 +0900

    세 자리 난수 생성 함수

--- jihyun/baseball.js
+++ jihyun/baseball.js
@@ -5,3 +5,16 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function threeRandomNumber() {
+  const numbers = Array.from({ length: 10 }, (_, index) => index.toString());
+  // const numbers = [...new Array(9)].map((_, i) => i + 1);
+  let randomNumbers = "";
+
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    randomNumbers += numbers.splice(randomIndex, 1)[0];
+  }
+
+  return randomNumbers;
+}
