47c5e198bb2adc515dbfae5a9cbb61b36c1de00d
torong <hi_ryuji@naver.com>
2023-12-14 14:22:23 +0900

    사용자가 입력한 값과 정답을 비교해 결과를 반환하는 함수

--- jihyun/baseball.js
+++ jihyun/baseball.js
@@ -6,6 +6,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+// 난수 생성 함수
 function threeRandomNumber() {
   const numbers = Array.from({ length: 10 }, (_, index) => index.toString());
   // const numbers = [...new Array(9)].map((_, i) => i + 1);
@@ -17,4 +18,38 @@ function threeRandomNumber() {
   }
 
   return randomNumbers;
+}
+
+// 결과 반환 함수
+function checkGuess(answer, guess) {
+  let strikes = 0;
+  let balls = 0;
+
+  const checkedIndexes = new Set();
+
+  // strikes
+  for (let i = 0; i < 3; i++) {
+    if (guess[i] === answer[i]) {
+      strikes++;
+      checkedIndexes.add(i);
+    }
+  }
+
+  // balls
+  for (let i = 0; i < 3; i++) {
+    if (!checkedIndexes.has(i)) {
+      if (answer.includes(guess[i])) {
+        balls++;
+      }
+    }
+  }
+
+  if (strikes === 3) {
+    return "3S";
+  }
+
+  const ballsStr = balls > 0 ? `${balls}B` : "0B";
+  const strikesStr = strikes > 0 ? `${strikes}S` : "0S";
+
+  return `${ballsStr}${strikesStr}`;
 }
